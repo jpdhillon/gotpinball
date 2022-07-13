762e82ccaa39cbd51d7858278f2e03174a3add38
JP Dhillon <jaipaldhillon@gmail.com>
2022-07-13 16:05:43 -0400

    Update security features to protect against noSQl injection and XSS

--- joiSchemas.js
+++ joiSchemas.js
@@ -1,4 +1,27 @@
-const Joi = require('Joi');
+const BaseJoi = require('Joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+      'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+      escapeHTML: {
+          validate(value, helpers) {
+              const clean = sanitizeHtml(value, {
+                  allowedTags: [],
+                  allowedAttributes: {},
+              });
+              if (clean !== value) return helpers.error('string.escapeHTML', { value })
+              return clean;
+          }
+      }
+  }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
